5ba674262a1e085982a0aea818901d13720c8303
Git-Dias <cloud.dias@outlook.com>
2025-07-30 17:36:54 -0300

    Cria o arquivo main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -1,1 +1,13 @@
+terraform {
 
+  # Declara as vesões que devem ser usadas nos providers
+  required_version = ">=1.0.0"
+
+  # Declara o provider da AWS e a versão que deve sera utilizada | Na configuração está definido que será utilizada especificamente a versão 5.35.0
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.35.0"
+    }
+  }
+}
