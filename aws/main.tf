896f2545b1927272eed55a2f66730b36339472d0
Git-Dias <cloud.dias@outlook.com>
2025-07-30 17:52:05 -0300

    Testa o scan

--- aws/main.tf
+++ aws/main.tf
@@ -9,12 +9,7 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.35.0"
     }
-  }
-  # A utilziação do bloco de backend aponta para um S3 / Storage Accout onde o state ficará armazenado, de forma remota nesses casos
-  backend "s3" {
-    bucket = "bucket-rstate-tf"          #nome do bucket
-    key    = "lab-env/terraform.tfstate" #path onde o tfstate será armazenado
-    region = "us-east-2"                 #região do bucket
-  }
-
+  }  
 }
+
+  # Declara o provider da AWS e a versão que deve sera utilizada | Na configuração está definido que será utilizada especificamente a versão 5.35.0
